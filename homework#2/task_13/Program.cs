d0b3d351dbec7502b8289eb6fb3a40a61b1f436f
ArturGert <arturgert89@yandex.ru>
2022-12-04 01:19:25 +0300

    Fix error on task_13

--- homework#2/task_13/Program.cs
+++ homework#2/task_13/Program.cs
@@ -1,19 +1,14 @@
-﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
-что третьей цифры нет.
-645 -> 5
-78 -> третьей цифры нет
-32679 -> 6*/
-
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или
+ сообщает, что третьей цифры нет. (645 -> 5, 78 -> третьей цифры нет, 32679 -> 6*/
+ 
 Console.Clear();
 Console.WriteLine("Введите число ");
 int N = Convert.ToInt32(Console.ReadLine());
-int ThreeDigitNumber = 0; 
-int ThirdDigit = 0;
+int FindNumber = N; 
 if (N>=100)
 {
     while (N>=1000) 
-    ThreeDigitNumber = (N /= 10);
-    ThirdDigit = (ThreeDigitNumber %10);
-    Console.Write($"Третья цифра заданного числа: {ThirdDigit}"); 
+    FindNumber = ((N /= 10) %10);
+    Console.Write($"Третья цифра заданного числа: {FindNumber}"); 
 }
 else Console.Write("В данном числе 3-я цифра отсутствует");
