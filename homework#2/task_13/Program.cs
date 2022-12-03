c4dfa15f7f81caffb8f11b41bedd49ac4c32aec6
ArturGert <arturgert89@yandex.ru>
2022-12-03 22:57:00 +0300

    task_13 add

---
+++ homework#2/task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int ThNumber = 0; 
+int ThirdDigit = 0;
+if (N>=100)
+{
+    while (N>=1000) 
+    ThNumber = (N /= 10);
+    ThirdDigit = (ThNumber %10);
+    Console.Write($"Третья цифра заданного числа: {ThirdDigit}"); 
+}
+else Console.Write("В введенном числе 3-я цифра отсутствует");
