f8403fe8821c95c30cc773fa40be373055029106
artyr_gert <arturgert@andex.ru>
2022-12-11 21:42:05 +0300

    add task_29

---
+++ homework#4/task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+Console.Clear();
+int[] newArr = new int [8];
+Random nums = new Random();
+for (int i = 0; i < newArr.Length; i++)
+{
+    newArr[i] = nums.Next(0, 99);
+}
+for (int i = 0; i < newArr.Length; i++)
+{
+    System.Console.Write(newArr[i] + " ");
+}
