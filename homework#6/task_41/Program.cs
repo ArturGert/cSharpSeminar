d4f799124339440e47fe191375438a9a6479fd49
artyr_gert <arturgert@andex.ru>
2022-12-16 22:21:59 +0300

    add task_41

--- homework#6/task_41/Program.cs
+++ homework#6/task_41/Program.cs
@@ -3,10 +3,20 @@
 1, -7, 567, 89, 223-> 3*/
 Console.Clear();
 Console.Write("Введите требуемое количество чисел через пробел: ");
-int[] n = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+int[] n = (Console.ReadLine() ?? "").Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
 int count = 0;
 for (int i = 0; i< n.Length; i++)
     if (n[i] > 0)
         count++;
-Console.WriteLine($"Количество чисел больше 0 составляет: {count}");
+Console.WriteLine($"Количество чисел больше 0 составляет: " + count);
+
+// Второй вариант решения
+// Console.Write("Введите требуемое количество чисел через пробел: ");
+// string str = Console.ReadLine() ?? " ";
+// int[] n = Array.ConvertAll(str.Split(' '), int.Parse);
+// int count = 0;
+// for (int i = 0; i< n.Length; i++)
+//     if (n[i] > 0)
+//         count++;
+// Console.WriteLine("Количество чисел больше 0 составляет: " + count);
 
